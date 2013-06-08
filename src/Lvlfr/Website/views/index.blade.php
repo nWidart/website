fc95d1f69130f8285c5326246bf26f65e3ceb261
Julien Tant <julien@ao6-labs.eu>
2013-06-08 13:41:44 +0200

    Debut de la homepage

--- src/Lvlfr/Website/views/index.blade.php
+++ src/Lvlfr/Website/views/index.blade.php
@@ -1,5 +1,30 @@
 @extends('base.layout')
 
 @section('content')
-<i class="icon-home"></i>Hello Blade :)
+<div class="fixedWithPage" id="indexPage">
+
+    <div class="bigColLeft" style="float:left; width:870px;">
+        <h2>Un framework PHP élégant, puissant et robuste</h2>
+
+        <p>
+            Disponible aujourd'hui dans sa quatrième version, le framework Laravel créé par <a href="https://github.com/taylorotwell" target="_blank">Taylor Otwell</a> en 2011 remporte un franc succès grâce à des atouts qui le rendent unique. Depuis le début, l'objectif du framework est de rendre le développement fun à nouveau, en fournissant aux développeurs les outils dont ils avaient besoin pour travailler efficacement sur leurs projets.
+        </p>
+    </div>
+
+    <div class="smallColRight" style="float:right; width: 300px;">
+
+        <a href="#" class="btn btn-orange fullWidth">Démarage rapide</a>
+
+        <h2>En direct du blog</h2>
+        <ul>
+            <li>Titre du billet <small>le xx/xx/xx</small></li>
+            <li>Titre du billet <small>le xx/xx/xx</small></li>
+            <li>Titre du billet <small>le xx/xx/xx</small></li>
+            <li>Titre du billet <small>le xx/xx/xx</small></li>
+            <li>Titre du billet <small>le xx/xx/xx</small></li>
+        </ul>
+        <a href="#">Accès au blog</a>
+    </div>
+
+</div>
 @stop
